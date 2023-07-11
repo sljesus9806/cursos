0be513649cd613d4b8647f4c3842255479b89559
sljesus9806 <sljesusmanuel@gmail.com>
2023-07-11 10:58:00 -0600

    Carpeta Imperativo

---
+++ Imperativo/maze.py
@@ -0,0 +1,51 @@
+import readchar
+import os
+
+POS_X = 0
+POS_Y = 1
+MAP_WIDTH = 20
+MAP_HEIGHT = 15
+
+my_position = [3, 1]
+
+#Draw map
+
+while True:
+    
+    print("+" + "-" * MAP_WIDTH * 3 + '+')
+
+    for coordinate_y in range(MAP_HEIGHT):
+        print ('|', end="")
+        for coordinate_x in range(MAP_WIDTH):
+            if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
+                print(" @ ", end = "")
+            else:
+                print('   ', end = '')
+        print('|')
+
+    print("+" + "-" * MAP_WIDTH * 3 + '+')
+
+    #direction = input('Donde te quieres mover WASD: ')
+
+    direction = readchar.readchar()
+
+    if direction == 'w':
+            my_position[POS_Y] -= 1
+            my_position[POS_Y] %= MAP_HEIGHT
+    elif direction == 's':
+            my_position[POS_Y] += 1
+            my_position[POS_Y] %= MAP_HEIGHT
+    elif direction == 'a':
+            my_position[POS_X] -= 1
+            my_position[POS_X] %= MAP_WIDTH
+    elif direction == 'd':
+            my_position[POS_X] += 1
+            my_position[POS_X] %= MAP_WIDTH
+    elif direction == 'q':
+        break
+    
+    
+    os.system("clear")
+
+
+
